bc83b0ed86098817f0ec9ae54c83c1f1476ce781
Sigrid Aasma <sigridaasma@gmail.com>
2018-12-12 23:46:02 +0200

    add responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Sidebar from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <Sidebar/>
         <main className={classes.content}>
             {props.children}
         </main>
